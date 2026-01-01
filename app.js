7db66e0059e88ae97f8ea7901745ee67a81c6abc
BlackHat86 <suryansh.c05@gmail.com>
2026-01-01 20:43:45 +0530

    changed app.js

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.get("/", (req, res) => {
 });
 
 // ✅ THIS is what Render scans for
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
