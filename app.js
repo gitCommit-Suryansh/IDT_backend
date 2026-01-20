d3913395af1eb69a30897b8f41812f725b0c2c56
BlackHat86 <suryansh.c05@gmail.com>
2026-01-20 23:50:04 +0530

    misc

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.get("/", (req, res) => {
 });
 
 // ✅ THIS is what Render scans for
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
